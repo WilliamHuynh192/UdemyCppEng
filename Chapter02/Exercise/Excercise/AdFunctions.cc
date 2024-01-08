4ccf7ba4a47f396614d6ff52be1c09049214f818
William Huynh <hy523029@dal.ca>
2024-01-08 18:12:51 -0400

    Finished Chapter 2 and its exercise

--- Chapter02/Exercise/Excercise/AdFunctions.cc
+++ Chapter02/Exercise/Excercise/AdFunctions.cc
@@ -4,3 +4,16 @@
 
 #include "AdConstants.h"
 #include "AdFunctions.h"
+
+float Ad::Utils::kph_to_mps(const float kph) {
+    return kph / 3.6F;
+}
+
+Ad::Types::VehicleType Ad::Data::init_ego_vehicle() {
+    auto vehicleType = Ad::Types::VehicleType{-1, Ad::Types::LaneAssociationType::Center, 135};
+    return vehicleType;
+}
+
+void Ad::Visualize::print_vehicle(const Ad::Types::VehicleType vehicle) {
+    std::printf("Id: %d, Speed: %d, Lane: %d\n", vehicle.id, vehicle.speed, vehicle.lane);
+}
--- Chapter02/Exercise/Excercise/AdTypes.h
+++ Chapter02/Exercise/Excercise/AdTypes.h
@@ -3,3 +3,24 @@
 #include <cstdint>
 
 #include "AdConstants.h"
+
+namespace Ad
+{
+namespace Types
+{
+enum class LaneAssociationType {
+    Unknown,
+    Left,
+    Center,
+    Right,
+};
+
+struct VehicleType
+{
+    int id;
+    LaneAssociationType lane;
+    int speed;
+    int relativeDistance;
+};
+}
+}
